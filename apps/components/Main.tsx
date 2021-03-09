f9c4f35aee012a6f95e5666538fda9ee617aae36
takaya787 <takaya318318@gmail.com>
2021-03-09 10:39:40 +0900

    calendarを表示

---
+++ apps/components/Main.tsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect, useContext } from 'react'
+import Calender from 'react-calendar'
+import 'react-calendar/dist/Calendar.css'
+import styles from './Main.module.css'
+
+export const Main: React.FC = () => {
+  //react-calendar用のstate
+  const [value, onChange] = useState<Date>(new Date());
+  const [activeDate, setActiveDate] = useState<Date>(new Date());
+
+  return (
+    <Calender
+      className={styles.main}
+      onChange={onChange}
+      // onClickDay={(value, event) => {
+      //   setIsEventForm(true);
+      // }}
+      onActiveStartDateChange={({ activeStartDate, value, view }) => setActiveDate(activeStartDate)}
+      tileClassName={styles.height}
+      // tileContent={({ date, view }) => getTileCircle(date, view)}
+
+      value={value}
+      showNeighboringMonth={false}
+      minDate={new Date(2000, 1, 1)}
+      maxDate={new Date(2100, 1, 1)}
+      // locale={"ja-JP"}
+      view={"month"}
+    />
+  )
+}
